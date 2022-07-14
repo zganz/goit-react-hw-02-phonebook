4cdaf9f62aff9af30456b81a2d57bcf3bc4cd686
zganz <humanmailer@gmail.com>
2022-07-14 18:59:53 +0300

    add contact

--- src/components/Contactlist/Contactlist.jsx
+++ src/components/Contactlist/Contactlist.jsx
@@ -1,18 +1,29 @@
-import css from './Contact.module.css';
+import PropTypes from 'prop-types';
+import { Contact } from '../Contact/Contact';
 
 export const ContactList = ({ contacts, handleDelete }) => {
   return (
     <ul>
       {contacts.map(({ name, number, id }) => (
-        <li key={id}>
-          <span>{`${name}: ${number}`}</span>
-          <span>
-            <button className={css.contact} onClick={() => handleDelete(id)}>
-              delete
-            </button>
-          </span>
-        </li>
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          handleDelete={handleDelete}
+        />
       ))}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  handleDelete: PropTypes.func.isRequired,
 };
